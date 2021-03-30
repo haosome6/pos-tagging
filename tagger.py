e2fc6307020bebf300eb8039e1dd89d9b55280ce
Haozhe <zhzjim@gmail.com>
2021-03-31 00:37:02 +0800

    Finish testing process

--- tagger.py
+++ tagger.py
@@ -4,6 +4,8 @@
 import os
 import sys
 
+all_pos = []
+
 
 def increment_table_count(key, table, increment_by=1):
     if key in table:
@@ -22,6 +24,47 @@ def update_conditional_table(table, sum_over_idx):
         increment_table_count(condition[sum_over_idx], count, times)
     for condition in table:
         table[condition] = table[condition] / count[condition[sum_over_idx]]
+
+
+def test_sentence(sentence, init_table, transit_table, emit_table):
+    """Given a sentence as a list, where each element is a word. Using three
+    tables to test the POS of the sentence. Return a list of POSs.
+    """
+    for i in range(len(sentence)):
+        word = sentence[i].lower()
+        if i == 0:
+            curr_p = {}
+            for pos in all_pos:
+                if pos in init_table and (word, pos) in emit_table:
+                    curr_p[pos] = \
+                        [init_table[pos] * emit_table[(word, pos)], [pos]]
+                else:
+                    curr_p[pos] = [0, [pos]]
+        else:
+            prev_p = curr_p
+            curr_p = {}
+            for pos in all_pos:
+                max_prob = 0
+                max_path = []
+                for last_pos, p in prev_p.items():
+                    if (last_pos, pos) in transit_table and (
+                    word, pos) in emit_table:
+                        prob = p[0] * transit_table[(last_pos, pos)] * \
+                               emit_table[(word, pos)]
+                    else:
+                        prob = 0
+                    if prob > max_prob:
+                        max_prob = prob
+                        max_path = p[1]
+                curr_p[pos] = [max_prob, max_path.append(pos)]
+
+    max_prob = 0
+    result_path = []
+    for p in curr_p.values():
+        if p[0] > max_prob:
+            max_prob = p[0]
+            result_path = p[1]
+    return result_path
 
 
 def tag(training_list, test_file, output_file):
@@ -45,20 +88,42 @@ def tag(training_list, test_file, output_file):
         lines = f.readlines()
 
         for line in lines:
+            # remove '\n'
+            line = line[:-1]
 
             last_word, last_pos = cur_word, cur_pos
             cur_word, cur_pos = line.split(" : ")
+            cur_word = cur_word.lower()
+            if cur_pos not in all_pos:
+                all_pos.append(cur_pos)
 
             if last_word in end_puncs and cur_word not in end_puncs:
                 increment_table_count(cur_pos, init_table)
             increment_table_count((last_pos, cur_pos), transit_table)
             increment_table_count((cur_word, cur_pos), emit_table)
 
+        f.close()
+
     init_count = sum(init_table.values())
     for pos in init_table:
         init_table[pos] = init_table[pos] / init_count
     update_conditional_table(transit_table, 0)
     update_conditional_table(emit_table, 1)
+
+    # Testing process
+    f = open(test_file)
+    output_f = open(output_file, "w")
+    lines = f.readlines()
+    sentence = []
+    for line in lines:
+        sentence.append(line)
+        if line in end_puncs:
+            result = test_sentence(sentence, init_table, transit_table,
+                                   emit_table)
+            for i in range(len(sentence)):
+                output_f.writelines([sentence[i], " : ", result[i], "\n"])
+    f.close()
+    output_f.close()
 
 
 if __name__ == '__main__':
